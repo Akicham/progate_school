30d7f70512db88ec87cd74be0ab1d3db04ead989
伊藤亜樹 <itoaki@itouaki-no-MacBook-Air.local>
2016-03-15 20:21:48 +0900

    コラム画像拡大縮小

--- develop/local.js
+++ develop/local.js
@@ -1,56 +1,48 @@
 $(function() {
 	// body...
 
-  $('#login-msg').click(function(){
- 
-   if($(this).hasClass('active')){
-    $(this).removeClass('active').text('もっと詳しく');
-    $('.top-message').fadeOut();
- 
-  }else{
+	$('#login-msg').click(function(){
+     if($(this).hasClass('active')){
+      $(this).removeClass('active').text('もっと詳しく');
+      $('.top-message').fadeOut();
 
-    $(this).addClass('active').text('閉じる');
-    $('.top-message').fadeIn();
-   
-  };
+     }else{
 
-   });
+      $(this).addClass('active').text('閉じる');
+      $('.top-message').fadeIn();
+     }
+
+	});
+
 
 
 $('.lesson-hover').hover(
+
 function(){
- $(this).find('p').fadeIn();
+  $(this).find('p').fadeIn();
+
 },
 
 function(){
- $(this).find('p').fadeOut();
-
+  $(this).find('p').fadeOut();
 
 }
 
 );
 
+
+$('.image-section').hover(
+  function(){
+   $(this).addClass('column-change');
+
+  },
+
+  function(){
+   $(this).removeClass('column-change');
+  });
+
+
+
 });
-  // $('.lesson-hover').hover(
-  //   function() {
-  //     // 子要素の「.text-contents」の要素を取得し、text-activeクラスをつけてください
-  //    $(this).find('.text-contents').addClass('text-active'); 
-      
-  //   },
-  //   function() {
-  //     // 子要素の「.text-contents」の要素を取得し、text-activeクラスを外してください
-  //     $(this).find('.text-contents').removeClass('text-active');
-      
-  //   }
-  // );
-
-// $('.lesson-hover').hover(function(){
-//   $(this).find('p').fadeIn();
-
-// },
-
-// {function(){
-// 	$(this).find('p').fadeOut();
-// };
-
+ 
 
